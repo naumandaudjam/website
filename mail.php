4d9995da8088a41326ad185568d589590f259ad3
naumandaudjam <74778430+naumandaudjam@users.noreply.github.com>
2021-06-04 22:58:54 +0500

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "naumandaudjam1996@gmail.com"
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@codeconia.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
